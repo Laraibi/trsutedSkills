00dd812b86a2b4465338c2a480ac0b140e10c934
Laraibi <meh.laraibi@gmail.com>
2022-01-28 16:39:42 +0100

    setup vuejs

---
+++ resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="container-lg container-fluid-sm">
+      <button class="btn btn-success"> Hello World</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+</style>
